2461a8f536df4dbad4bb9feed0790f3a2cf442c2
Maurice Ling <maurice@linglink.com>
2016-07-11 00:48:42 -0700

    Remove class examples

--- tools/graphing/plotAngles.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-feedgnuplot --lines --points --legend 0 "Pitch" --legend 1 "Roll" --title "Board Attitude" --stream --xlen 10 --ylabel "Angle"
--- tools/graphing/plotXYZ.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-feedgnuplot --lines --points --legend 0 "X" --legend 1 "Y" --legend 2 "Z" --title "Accelerometer Data" --stream --xlen 10 --ylabel "Measurement"
